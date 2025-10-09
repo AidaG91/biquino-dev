ec501a6bffb67b15363fa2b04e09bd17d48c473d
AidaG91 <aida.garmus@gmail.com>
2025-10-09 15:21:05 +0200

    Header created

--- biquino/src/App.jsx
+++ biquino/src/App.jsx
@@ -1,9 +1,15 @@
 import AppRouter from "./routes/AppRouter";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
-      <AppRouter />
+      <Header />
+      <main>
+        <AppRouter />
+      </main>
+
+      {/* Aquí irá el Footer <Footer /> */}
     </>
   );
 }
