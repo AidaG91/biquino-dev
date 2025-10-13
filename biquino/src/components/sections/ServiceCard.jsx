6ec754660bc48a87e44c508962e793fb8d2a17df
AidaG91 <aida.garmus@gmail.com>
2025-10-13 13:11:30 +0200

    Services section styled

--- biquino/src/components/sections/ServiceCard.jsx
+++ biquino/src/components/sections/ServiceCard.jsx
@@ -5,7 +5,7 @@ export default function ServiceCard({ data }) {
   const { icon, title, text } = data;
 
   return (
-    <div className={styles.ServiceCard}>
+    <div className={styles.serviceCard}>
       <img src={icon} alt={title} className={styles.cardIcon} />
       <h2 className={styles.cardTitle}>{title}</h2>
       <p className={styles.cardText}>{text}</p>
