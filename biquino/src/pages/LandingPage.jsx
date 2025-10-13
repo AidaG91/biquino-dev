0843da881b111cf93670a74361b7fe46c6ffd9d0
AidaG91 <aida.garmus@gmail.com>
2025-10-13 12:10:21 +0200

    Hero added

--- biquino/src/pages/LandingPage.jsx
+++ biquino/src/pages/LandingPage.jsx
@@ -1,5 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Hero from "../components/sections/Hero";
+import ServicesSection from "../components/sections/ServicesSection";
+import ProjectsSection from "../components/sections/ProjectsSection";
+import CallToAction from "../components/sections/CallToAction";
+import ContactForm from "../components/sections/ContactForm";
 
 export default function LandingPage() {
-    // ... tu código de Landing Page
+  return (
+    <article>
+      <Hero />
+      <ServicesSection />
+      <ProjectsSection />
+      <CallToAction />
+      <ContactForm />
+    </article>
+  );
 }
