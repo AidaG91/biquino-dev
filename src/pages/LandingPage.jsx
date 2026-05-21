27bd55fec23a49474c2448f1d829328e53b3009d
AidaG91 <aida.garmus@gmail.com>
2026-05-21 23:12:20 +0200

    cleanup: delete deade code and unused component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,16 +1,14 @@
-import { Link } from "react-router-dom";
-import Menu from "../components/sections/Menu";
-import ServicesSection from "../components/sections/ServicesSection";
-import ProjectsSection from "../components/sections/ProjectsSection";
 import CallToAction from "../components/sections/CallToAction";
 import ContactForm from "../components/sections/ContactForm";
+import Menu from "../components/sections/Menu";
+import ServicesSection from "../components/sections/ServicesSection";
 
 export default function LandingPage() {
   return (
     <article>
       <Menu />
       <ServicesSection />
-      <ProjectsSection />
+    
       <CallToAction
         variant="light"
         title="Gran variedad de materiales con infinidad de aplicaciones"
